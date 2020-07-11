ae9ddf42dbc09a4863a7ccf03ae51ef12762f679
ahnaf000 <66615579+ahnaf000@users.noreply.github.com>
2020-07-11 14:58:40 +0600

    work in progress with search

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -8,3 +8,12 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def searchResult(request, title):
+    title = request.POST['q']
+    submitBtn = request.POST['submitBtn']
+
+
+    return render(request, "encyclopedia/searchResult.html",{
+        'entry': util.get_entry(title),
+        'title': title
+    })
